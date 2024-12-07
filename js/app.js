3031b94dea4e9cf16d3063dbcaa5d7721d12e893
marufalaslam <maruf14@cse.pstu.ac.bd>
2024-12-07 18:17:06 +0600

    updated

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$('.slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: false,
+    cssEase: 'linear',
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000
+})
